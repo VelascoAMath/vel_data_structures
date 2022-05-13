44c1e3a38f26778888a7c4d0d335de1a523cda13
VelascoAMath <alfredo0234@gmail.com>
2022-05-12 20:13:10 -0400

    Added a setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='vel_data_structures',
+    name='Velasco\' Python Structures',
     version='0.0.1',
     author='Alfredo Velasco',
     author_email='avelasco@princeton.edu',
@@ -16,6 +16,6 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/VelascoAMath/vel_data_structures/issues"
     },
     license='MIT',
-    packages=['PythonDataStructures'],
+    packages=['vel_data_structures'],
     install_requires=['requests'],
 )
