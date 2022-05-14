3ae061fea667356beaa572d6b9d5adbd4e6dce8b
VelascoAMath <alfredo0234@gmail.com>
2022-05-14 00:31:46 -0400

    Huge release! Five different data structures with full documentation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='Velasco\' Python Structures',
-    version='0.0.1',
+    version='1.0.0',
     author='Alfredo Velasco',
     author_email='avelasco@princeton.edu',
     description='Testing installation of Package',
