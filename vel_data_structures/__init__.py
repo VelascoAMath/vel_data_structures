fca396f5150d8e9dc7e820cee793ebad4ce3a7b2
VelascoAMath <alfredo0234@gmail.com>
2022-05-17 14:47:25 -0400

    Trying to fix import errors

--- vel_data_structures/__init__.py
+++ vel_data_structures/__init__.py
@@ -1,11 +1,18 @@
-# # https://stackoverflow.com/a/44230992
-# import os
-# import sys
 
-# file_dir = os.path.dirname(__file__)
-# sys.path.append(file_dir)
-
-
-from .BTree import BTree
-from .AVL import AVL
-from .AVL_Dict import AVL_Dict
+def main():
+    """
+    """
+    # https://stackoverflow.com/a/44230992
+    import os
+    import sys
+    
+    file_dir = os.path.dirname(__file__)
+    sys.path.append(file_dir)
+    
+    
+    from .BTree import BTree
+    from .AVL import AVL
+    from .AVL_Dict import AVL_Dict
+    
+if __name__ == "__main__":
+    main()
