dcaa44d67560aa2284892d7ada9b1405532e7917
VelascoAMath <alfredo0234@gmail.com>
2022-05-17 14:08:09 -0400

    Trying to fix import errors

--- vel_data_structures/__init__.py
+++ vel_data_structures/__init__.py
@@ -4,6 +4,6 @@ print("Hey! Why aren't you working!!!")
 import inspect, testpkg
 print(inspect.getmembers(testpkg, inspect.ismodule))
 
-from BTree import BTree
-from AVL import AVL, _Node
-from AVL_Dict import AVL_Dict
+# from BTree import BTree
+# from AVL import AVL, _Node
+# from AVL_Dict import AVL_Dict
