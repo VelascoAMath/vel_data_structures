1e2ca68ba047a77089999b4ccb6b6fd07b967f14
VelascoAMath <alfredo0234@gmail.com>
2022-05-17 14:56:50 -0400

    Trying to fix import errors

--- vel_data_structures/__init__.py
+++ vel_data_structures/__init__.py
@@ -1,4 +1,4 @@
-
+# For some reason, Python gives import errors when tryingt to import from the current directory. Although a bit hack, this should fix those issues
 # https://stackoverflow.com/a/44230992
 import os
 import sys
