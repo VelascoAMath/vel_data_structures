9ce352a0698cc47736f1be38900bd859ab8192e0
VelascoAMath <alfredo0234@gmail.com>
2022-05-17 14:45:24 -0400

    Trying to fix import errors

--- vel_data_structures/__init__.py
+++ vel_data_structures/__init__.py
@@ -1,17 +1,10 @@
-# https://stackoverflow.com/a/44230992
-import os
-import sys
+# # https://stackoverflow.com/a/44230992
+# import os
+# import sys
 
-file_dir = os.path.dirname(__file__)
-sys.path.append(file_dir)
+# file_dir = os.path.dirname(__file__)
+# sys.path.append(file_dir)
 
-
-
-
-
-# from BTree import BTree
-# from AVL import AVL
-# from AVL_Dict import AVL_Dict
 
 from .BTree import BTree
 from .AVL import AVL
