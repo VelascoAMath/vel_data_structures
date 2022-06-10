97331289701e9d623de58a2d3b04e0183589830f
VelascoAMath <alfredo0234@gmail.com>
2022-06-10 18:58:54 -0400

    _KeyVal how has a <= and >= implemented

--- vel_data_structures/AVL_Dict.py
+++ vel_data_structures/AVL_Dict.py
@@ -22,8 +22,14 @@ class _KeyVal(object):
 	def __lt__(self, other):
 		return self.key < other.key
 
+	def __le__(self, other):
+		return self.key <= other.key
+
 	def __gt__(self, other):
 		return self.key > other.key
+
+	def __ge__(self, other):
+		return self.key >= other.key
 
 	def __eq__(self, other):
 		return self.key == other.key
