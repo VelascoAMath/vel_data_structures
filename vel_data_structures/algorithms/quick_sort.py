cf8bc062efdbae362ed7f93787ec77e3dc88d5ee
Alfredo Velasco <avelasco@mcelroy.com>
2023-10-23 14:21:56 -0500

    Added the quicksort algorithm

---
+++ vel_data_structures/algorithms/quick_sort.py
@@ -0,0 +1,111 @@
+import itertools
+
+from tqdm import tqdm
+
+
+def _partition(a, lo=None, hi=None):
+	'''
+	lo: the inclusive lower index of the range one wishes to partition
+	hi: the exclusive higher index of the range one wishes to partition
+	'''
+	def swap(l, i, j):
+		temp = l[i]
+		l[i] = l[j]
+		l[j] = temp
+
+	# Empty and singular arrays are partitioned already
+	if len(a) <= 1:
+		return
+
+	if hi is None:
+		hi = len(a)
+	if lo is None:
+		lo = 0
+
+	# Empty and singular arrays are already partitioned
+	if hi - lo <= 1:
+		return
+
+	pivot_index = hi - 1
+	pivot = a[pivot_index]
+
+	i = lo
+	j = hi - 2
+
+	while True:
+		# Find some elements whose order is reversed
+		while i < pivot_index and a[i] <= pivot:
+			i += 1
+		while j > 0 and pivot <= a[j]:
+			j -= 1
+
+		# Done. Just need to play the pivot in place
+		if i >= j:
+			swap(a, i, pivot_index)
+			return
+
+		# No issue. Let's move on
+		if a[i] <= pivot and pivot <= a[j]:
+			i += 1
+			j -= 1
+		# Reversed positions
+		elif a[j] < pivot and pivot < a[i]:
+			swap(a, i, j)
+			i += 1
+			j += 1
+
+
+def partition_test():
+	for size in tqdm(range(10 + 1)):
+		l = list(range(1, size + 1))
+		for x in itertools.permutations(l):
+			a = list(x)
+			if len(a) > 0:
+				pivot = a[-1]
+			_partition(a)
+			if len(a) > 0:
+				pivot_index = a.index(pivot)
+				for i in range(pivot_index):
+					if a[i] > pivot:
+						raise Exception(f"{size=} {x=} {a=}")
+				for i in range(pivot_index + 1, len(a)):
+					if a[i] < pivot:
+						raise Exception(f"{size=} {x=} {a=}")
+
+
+def quicksort(a, lo=None, hi=None):
+	if (len(a) <= 1):
+		return
+	if lo is None:
+		lo = 0
+	if hi is None:
+		hi = len(a)
+
+	if hi - lo <= 1:
+		return
+
+	pivot = a[hi-1]
+	_partition(a, lo, hi)
+	pivot_index = a.index(pivot)
+	if( lo < pivot_index):
+		quicksort(a, lo, pivot_index)
+	if (pivot_index < hi):
+		quicksort(a, pivot_index, hi)
+
+
+def quicksort_test():
+	for size in tqdm(range(10 + 1)):
+		l = list(range(1, size + 1))
+		for x in itertools.permutations(l):
+			a = list(x)
+			quicksort(a)
+			if len(a) > 1:
+				for i in range(1, len(a)):
+					if a[i - 1] > a[i]:
+						raise Exception(f"{size=} {x=} {a=}")
+
+
+
+if __name__ == '__main__':
+	partition_test()
+	quicksort_test()
