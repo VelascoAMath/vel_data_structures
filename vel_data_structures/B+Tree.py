d5b3af674f95140cd36e3946e698b6377bcbb9dd
Alfredo Velasco <avelasco@mcelroy.com>
2023-11-09 13:30:26 -0600

    Created a basic B+ tree which only allows for insertions and the printing of itself

---
+++ vel_data_structures/B+Tree.py
@@ -0,0 +1,113 @@
+from dataclasses import dataclass, field
+
+from vel_data_structures import List_Heap
+
+
+@dataclass(order=True)
+class _Node:
+    b: int = 3
+    items: List_Heap = field(default_factory=lambda: List_Heap(min=False))
+    children: List_Heap = field(default_factory=lambda : List_Heap(min=False))
+
+    def __post_init__(self):
+        self.next = None
+
+    def __getitem__(self, key):
+        return self.items.__getitem__(key)
+
+    def insert(self, item):
+        self.items.insert(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def pop_child(self):
+        return self.children.pop()
+    def is_internal(self):
+        return len(self.children) != 0
+    def is_stuffed(self):
+        return len(self.items) >= self.b
+
+    def insert_child(self, child):
+        self.children.insert(child)
+
+    def __repr__(self):
+        return f"_Node(b={self.b}, items={self.items}" + (f", children={self.children}" if len(self.children) > 0 else "") + ")"
+
+@dataclass
+class BPTree:
+    b: int = 3
+    n: int = 0
+    root: _Node = None
+
+    def _find_item(self, item):
+        path = [self.root]
+        curr = self.root
+
+        while curr.is_internal():
+            if item < curr.items[0]:
+                curr = curr.children[0]
+            elif item >= curr.items[-1]:
+                curr = curr.children[-1]
+            else:
+                for i in range(len(curr.items) - 1):
+                    if curr[i] <= item <= curr[i + 1]:
+                        curr = curr.children[i]
+                        break
+            path.append(curr)
+
+        return path
+
+    def insert(self, item):
+        if self.n == 0:
+            self.root = _Node()
+
+        path = self._find_item(item)
+        curr = (path[-1])
+        curr.insert(item)
+        self.n += 1
+
+        while curr.is_stuffed():
+            if curr != self.root:
+                new_sibling = _Node(self.b)
+                curr.next = new_sibling
+                new_sibling.insert(curr.items.pop())
+
+                parent = path.pop()
+                parent.insert_child(curr)
+                parent.insert_child(new_sibling)
+                parent.insert(new_sibling[0])
+            else:
+                self.root = _Node(self.b)
+                new_sibling = _Node(self.b)
+                curr.next = new_sibling
+
+                new_sibling.insert(curr.pop())
+                self.root.insert_child(curr)
+                self.root.insert_child(new_sibling)
+                self.root.items.insert(new_sibling[0])
+
+    def __str__(self):
+        curr = self.root
+        while curr.is_internal():
+            curr = curr.children[0]
+
+        result = []
+        while curr is not None:
+            result.extend(curr.items)
+            curr = curr.next
+        return str(result)
+
+
+
+def main():
+    b = BPTree()
+    for i in range(100):
+        b.insert(i)
+
+    print(f"{b=}")
+    print(b)
+
+
+if __name__ == "__main__":
+    main()
